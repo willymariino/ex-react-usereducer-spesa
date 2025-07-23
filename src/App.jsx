72bbca9d4c4681998a2473379db8b2a388cb1bd9
willymariino <wg.mariino@gmail.com>
2025-07-23 15:08:29 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState, useReducer } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [item, setItem] = useState("")
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>lista della spesa</h1>
+
+      <ul>
+        {products.map((product, index) => (
+
+          <li key={index}>
+            {product.name}: {product.price}
+
+
+          </li>
+
+        ))}
+
+      </ul>
+
     </>
   )
 }
